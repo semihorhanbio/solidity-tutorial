982258332b6308ce93e8b499828270b4592041bd
Semih Orhan <sorhan3.14@gmail.com>
2024-01-15 16:53:01 +0300

    change rinkby to sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    rinkeby: {
+    sepolia: {
       url: process.env.NODE_API_URL,
       accounts: [process.env.SEPOLIA_PRIVATE_KEY],
     },
