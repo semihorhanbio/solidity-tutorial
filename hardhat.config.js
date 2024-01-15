a945d64143699f50c6b6afb49a298c742833418a
Semih Orhan <sorhan3.14@gmail.com>
2024-01-15 16:52:33 +0300

    Update state using a transaction

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: process.env.NODE_API_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
--- scripts/start.js
+++ scripts/start.js
@@ -1,12 +1,29 @@
 const hre = require("hardhat");
 async function main() {
+  const [owner, somebodyElse] = await hre.ethers.getSigners();
   const keyboardsContract = await hre.ethers.deployContract("Keyboards");
   await keyboardsContract.waitForDeployment();
 
   console.log("Contract deployed to:", keyboardsContract.target);
 
-  const keyboards = await keyboardsContract.getKeyboards();
+  let keyboards = await keyboardsContract.getKeyboards();
   console.log("We got the keyboards!", keyboards);
+
+  const keyboardTxn1 = await keyboardsContract.create(
+    "A really great keyboard!"
+  );
+  await keyboardTxn1.wait();
+
+  const keyboardTx2 = await keyboardsContract
+    .connect(somebodyElse)
+    .create("An even better keyboard!");
+  await keyboardTx2.wait();
+
+  keyboards = await keyboardsContract.getKeyboards();
+  console.log("We got the keyboards!", keyboards);
+
+  keyboards = await keyboardsContract.connect(somebodyElse).getKeyboards();
+  console.log("And as somebody else!", keyboards);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
